cbf2dbb7e91696c6b13945514fc0364dc58c0b03
Nguyen Thanh Tinh <ngthanhtinh801@gmail.com>
2024-05-10 09:28:49 +0700

    update view

--- QLKB_VIEW.sql
+++ QLKB_VIEW.sql
@@ -14,3 +14,33 @@ FROM Receptionist;
 CREATE VIEW labphysician_VIEW AS
 SELECT LabPhysID, LabPhysName, Gender, LabPhysBirthdate, DateJoined, Specialization, LabPhysPhone, LabPhysEmail
 FROM LaboratoryPhysician;
+
+/*view để xem thông tin*/
+/*Danh sách bệnh nhân*/
+CREATE VIEW patient_list_VIEW AS
+SELECT PatientID, CitizenID, PatientName, Gender, PatientBirthdate, PatientPhone, PatientEmail, PatientAddr, EmergencyContact
+FROM Patient;
+
+/*Danh sách thuốc sắp hết hàng trong kho (xét số lượng <= 50)*/
+CREATE VIEW medicine_low_quantity_VIEW AS
+SELECT MedID, MedName, Unit, Quantity
+FROM Medicine
+WHERE Quantity <= 50;
+
+/*View để kết hợp dữ liệu từ nhiều bảng*/
+/*Hiển thị đầy đủ thông tin thuốc về thuốc*/
+CREATE VIEW medicine_details_VIEW AS
+SELECT MedID, MedName, MedDesc, Unit, PurchasePrice, Price, Quantity, Medicine.ManufID, ManufName
+FROM Medicine JOIN MedicineManufaturer ON Medicine.ManufID = MedicineManufaturer.ManufID;
+
+/*Hiển thị chi tiết đơn thuốc của các lần khám bệnh*/
+CREATE VIEW prescription_VIEW AS
+SELECT Consultation.ConsultationID, PatientName, DoctorName, EndTime, Consultation_Medicine.MedID, MedName, Consultation_Medicine.Quantity, Unit
+FROM Consultation
+JOIN Patient ON Consultation.PatientID = Patient.PatientID
+JOIN Doctor ON Consultation.DoctorID = Doctor.DoctorID
+JOIN Consultation_Medicine ON Consultation.ConsultationID = Consultation_Medicine.ConsultationID
+JOIN Medicine ON Consultation_Medicine.MedID = Medicine.MedID
+ORDER BY ConsultationID;
+
+ 
